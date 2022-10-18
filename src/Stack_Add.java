5f0895ededc032e7fe359898a1668f0015554cb0
aoshinbm <aoshinbm28797@gmail.com>
2022-10-18 23:36:51 +0530

    Added:Create Stack and add elements

--- src/Stack_Add.java
+++ src/Stack_Add.java
@@ -5,8 +5,18 @@ public class Stack_Add {
         System.out.println("Stack:");
         Stack stack=new Stack();
         stack.push(70);
+        System.out.println(stack);
         stack.push(30);
+        System.out.println(stack);
         stack.push(56);
         System.out.println(stack);
+
+        Stack stack1=new Stack();
+        stack1.push(56);
+        System.out.println(stack1);
+        stack1.push(30);
+        System.out.println(stack1);
+        stack1.push(70);
+        System.out.println(stack1);
     }
 }
