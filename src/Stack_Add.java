c79e318af67be79d84fc79685914d3e7c8787e33
aoshinbm <aoshinbm28797@gmail.com>
2022-10-20 21:23:48 +0530

    Added:Create Stack and add elements

--- src/Stack_Add.java
+++ src/Stack_Add.java
@@ -1,22 +1,55 @@
 import java.util.Stack;
 
 public class Stack_Add {
-    public static void main(String[] args) {
-        System.out.println("Stack:");
-        Stack stack=new Stack();
-        stack.push(70);
-        System.out.println(stack);
-        stack.push(30);
-        System.out.println(stack);
-        stack.push(56);
-        System.out.println(stack);
+    Node head;
 
-        Stack stack1=new Stack();
-        stack1.push(56);
-        System.out.println(stack1);
-        stack1.push(30);
-        System.out.println(stack1);
-        stack1.push(70);
-        System.out.println(stack1);
+    class Node {
+        int data;
+        Node ref;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else if (head.ref == null) {
+            head.ref = newNode;
+        } else {
+            Node temp = head;
+            while (temp.ref != null) {
+                temp = temp.ref;
+            }
+            temp.ref = newNode;
+        }
+    }
+    void print(){
+        if (head==null){
+            System.out.println("Linked List is empty");
+        }
+        else{
+            Node temp=head;
+            while (temp!=null){
+                if (temp.ref!=null){
+                    System.out.println("["+temp.data+"]");
+                }
+                else {
+                    System.out.println("["+temp.data+"]");
+                }
+                temp=temp.ref;
+            }
+        }
+    }
+    public static void main(String[] args) {
+        Stack_Add stack=new Stack_Add();
+        System.out.println("Stack ");
+        stack.addLast(70);
+        stack.addLast(30);
+        stack.addLast(56);
+        stack.print();
+
     }
 }
